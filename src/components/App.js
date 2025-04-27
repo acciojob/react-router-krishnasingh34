aa59d7105a80099da596a80aa8ea9d092ee09045
Krishna Singh <144099384+krishnasingh34@users.noreply.github.com>
2025-04-27 23:14:59 +0530

    submission

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
 import React from "react";
 import './../styles/App.css';
+import {Routes, Route} from 'react-router-dom'
+import Home from "./Home";
+import About from "./About";
+import Navbar from "./Navbar";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Navbar />
+        <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+      </Routes>
     </div>
   )
 }
