31f3bf5dfb93a7dbbce5730be66bd58652c0e191
Krishna <singhkrishna9689@gmail.com>
2025-04-28 20:59:20 +0530

    subbb

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,10 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+      </ul>  
     </div>
   )
 }
