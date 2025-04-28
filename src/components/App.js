675429e92796ac263bd78bf689ab07410738b9d1
Krishna <singhkrishna9689@gmail.com>
2025-04-28 20:27:41 +0530

    sub

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import './../styles/App.css';
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from "./Home";
 import About from "./About";
 import Navbar from "./Navbar";
@@ -10,10 +10,12 @@ const App = () => {
   return (
     <div>
       <Navbar />
+      <BrowserRouter>
         <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/about' element={<About />}/>
-      </Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
